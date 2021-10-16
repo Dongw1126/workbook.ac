f22d13f3b5c2ff6fd7f2f0da504aef4d051e0a43
Dongw1126 <topblade71@naver.com>
2021-10-16 17:09:57 +0900

    feat: add dark mode

--- client/src/components/MainComponent.tsx
+++ client/src/components/MainComponent.tsx
@@ -1,17 +1,35 @@
+import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import CreateWorkbook from "../pages/CreateWorkbook"
 import Info from "../pages/Info"
 import Nav from "../pages/Nav"
 
 function MainComponent() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
     return (
-        <BrowserRouter>
-            <Nav/>
-            <Switch>
-                <Route exact path="/" component={CreateWorkbook} />
-                <Route exact path="/info" component={Info} />
-            </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={CreateWorkbook} />
+                    <Route exact path="/info" component={Info} />
+                </Switch>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
