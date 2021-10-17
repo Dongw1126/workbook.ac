15ec4734f090553f6e13193e8f1b791a45fba017
Dongw1126 <topblade71@naver.com>
2021-10-18 00:27:38 +0900

    feat: add Pretendard font

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './static/fonts/Pretendard/pretendard.css';
 
 ReactDOM.render(
   <React.StrictMode>
