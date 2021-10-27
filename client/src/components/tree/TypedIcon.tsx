ef954cb3a4e55714393c61b6fb9f4f06e4c39b85
Dongw1126 <topblade71@naver.com>
2021-10-27 22:12:18 +0900

    fix: delete unnecessary an if-statement

--- client/src/components/tree/TypedIcon.tsx
+++ client/src/components/tree/TypedIcon.tsx
@@ -14,21 +14,19 @@ export const TypedIcon: React.FC<Props> = (props) => {
     return <FolderIcon />;
   }
   else {
-    if (props.level) {
-      const tierUrl = Constants.SERVER_URL + "tier/svg/" + props.level + ".svg";
-      return (
-        <Box>
-          <img src={tierUrl} 
-          alt="tier image"
-          style={{
-            width:"1,2em", 
-            height:"1.2em", 
-            lineHeight:'inherit', 
-            verticalAlign:'middle'
-          }}/>
-        </Box>
-      );
-    }
+    const tierUrl = Constants.SERVER_URL + "tier/svg/" + props.level + ".svg";
+    return (
+      <Box>
+        <img src={tierUrl} 
+        alt="tier image"
+        style={{
+          width:"1,2em", 
+          height:"1.2em", 
+          lineHeight:'inherit', 
+          verticalAlign:'middle'
+        }}/>
+      </Box>
+    );
   }
   return null;
 };
