47b18fa74573cad0817d8a82475403dc760090ba
Dongw1126 <topblade71@naver.com>
2021-10-29 20:57:11 +0900

    chore: add missing file

---
+++ client/src/App.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+test("", () => {
+    const a = 1;
+    expect(a).toBe(1);
+});
--- client/src/tests/NavContent.test.tsx
+++ client/src/tests/NavContent.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "../pages/Nav";
+import '@testing-library/jest-dom';
 
 test("Navigation Bar Content", () => {
     const nav = render(
