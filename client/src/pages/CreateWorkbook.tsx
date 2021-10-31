28b76f3101adaf93ca3c17765b2f22c7943a8012
Dongw1126 <topblade71@naver.com>
2021-10-31 19:41:02 +0900

    feat: resizable split

--- client/src/pages/CreateWorkbook.tsx
+++ client/src/pages/CreateWorkbook.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Split from 'react-split'
 import WorkbookComponent from '../components/WorkbookComponent';
 import SearchComponent from '../components/SearchComponent';
 
 function CreateWorkbook() {
     return (
+        <Split
+            className="split"
+            sizes={[40, 60]}
+            minSize={300}
+            gutterSize={10}
+            cursor="col-resize"
+        >
+            <div style={{ margin: 10 }}>
+                <WorkbookComponent/>
+            </div>
+            <div style={{ margin: 10 }}>
+                <SearchComponent/>
+            </div>
+        </Split>
+    );
+}
+
+export default CreateWorkbook;
+
+/*
         <div style={{ width: "100%" }}>
             <div style={{ width: "40%", float: "left", boxSizing: "border-box" }}>
                 <div style={{ margin: "5%" }}>
@@ -16,7 +37,4 @@ function CreateWorkbook() {
                 </div>
             </div>
         </div>
-    );
-}
-
-export default CreateWorkbook;
+*/
