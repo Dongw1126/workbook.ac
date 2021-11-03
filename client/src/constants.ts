b7f8b6ece33daa1fe91474a81ee1d600d8c4c334
Dongw1126 <topblade71@naver.com>
2021-11-03 15:07:18 +0900

    chore: add limitation constants

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,9 @@
 export const SERVER_URL = "http://localhost:5000/";
 
 export const TREE_ITEM_SPACE = 36;
+
+export const MAX_TREE_ID = 200;
+
+export const MAX_FOLDER_NUM = 100;
+
+export const MAX_WORKBOOK_NUM = 100;
