d181d0bc764f89fe24a82af736f5c425a43667e2
Dongw1126 <topblade71@naver.com>
2021-11-06 05:53:38 +0900

    fix: tree placeholer width

--- client/src/components/tree/Placeholder.tsx
+++ client/src/components/tree/Placeholder.tsx
@@ -9,9 +9,15 @@ type Props = {
 }
 
 function Placeholder(props: Props) {
-    const left = props.depth * Constants.TREE_ITEM_SPACE;
+    // const left = props.depth * Constants.TREE_ITEM_SPACE;
+    const l_width = props.depth * Constants.TREE_ITEM_SPACE;
+    const r_width = "calc(100% - " + l_width + "px)"; 
+    // const r_width = (100 - props.depth * 10) + "%";
     return (
-        <div className={styles.root} style={{ left }}></div>
+        <div className={styles.container}>
+            <div className={styles.spacing} style={{ width: l_width }}></div>
+            <div className={styles.main} style={{ width: r_width }}></div>
+        </div>
     );
 }
 
