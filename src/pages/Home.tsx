3f2238025d78048638e2caee76754d7e3e1af9e2
Dongw1126 <topblade71@naver.com>
2021-11-18 02:42:52 +0900

    feat: add usage template

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSpring, animated } from 'react-spring';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import styles from "./Home.module.css";
 
 function Home() {
+    const alignCenter = { display: 'flex', alignItems: 'center' }
     const parallax = useRef<IParallax>(null!);
 
     const logoProps = useSpring({
@@ -12,13 +13,8 @@ function Home() {
         config: { mass: 2 }
     });
 
-    const arrowProps = useSpring({
-        to: { opacity: 1, marginBottom: 0 },
-        from: { opacity: 0.3, marginBottom: 50 },
-    });
-
     return (
-        <Parallax ref={parallax} pages={3} >
+        <Parallax ref={parallax} pages={6} style={{ height: "90%" }}>
             <ParallaxLayer
                 offset={0}
                 speed={0.1}
@@ -31,30 +27,55 @@ function Home() {
 
             <ParallaxLayer
                 offset={1}
-                speed={0.4}
-                >
-                    <div className={styles.blackBar}></div>
+                speed={0.5}
+            >
+                <div className={styles.blackBar}></div>
             </ParallaxLayer>
 
             <ParallaxLayer
-                factor={0.5}
+                factor={0.8}
                 offset={1}
-                speed={0.5}
+                speed={0.6}
                 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <div className={styles.content}>
-                    WORKBOOK.AC는 <a href="#">solved.ac</a>를 기반으로 만들어진 문제집 제작, 공유 플랫폼입니다.<br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/>
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br/>
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+                    <p>
+                        WORKBOOK.AC는 <a href="#">solved.ac</a>를 기반으로 만들어진 문제집 제작, 공유 플랫폼입니다.
+                    </p>
+                    원하는 문제를 골라 문제집을 만들어보고 <br />
+                    사람들과 함께 공유해보세요. <br />
+                    좋은 문제집을 발견했다면 좋아요도 눌러보세요.<br />
                 </div>
             </ParallaxLayer>
 
-            <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
+            <ParallaxLayer sticky={{ start: 2, end: 4 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+                <div className={styles.sticky}>
+                    <div className={styles.circle}></div>
+                    <div className={styles.subTitle}>사용법</div>
+                </div>
+            </ParallaxLayer>
 
+
+            <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                <div className={`${styles.usage} ${styles.content}`}>
+                    asdasdasdasd
+                </div>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={3.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                <div className={`${styles.usage} ${styles.content}`}>
+                    asdadasdads
+                </div>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={4.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                <div className={`${styles.usage} ${styles.content}`}>
+                    asdadasdads
+                </div>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={5} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
             <ParallaxLayer
-                offset={2}
+                offset={5}
                 speed={0.5}
                 style={{
                     display: 'flex',
@@ -69,14 +90,3 @@ function Home() {
 }
 
 export default Home;
-
-/*
-        <div style={{ textAlign: "center" }}>
-            <animated.div style={logoProps}>
-                <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="" />
-            </animated.div>
-            <div className={styles.content}>
-                WORKBOOK.AC는 <a href="#">solved.ac</a>를 기반으로 만들어진 문제집 제작, 공유 플랫폼입니다.
-            </div>
-        </div>
-*/
