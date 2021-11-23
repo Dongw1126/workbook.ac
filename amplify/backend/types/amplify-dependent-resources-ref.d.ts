15869a763dbb3b35a81f8210c6a0176ec79c7061
Dongw1126 <topblade71@naver.com>
2021-11-23 22:57:30 +0900

    feat: amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "workbookac": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
