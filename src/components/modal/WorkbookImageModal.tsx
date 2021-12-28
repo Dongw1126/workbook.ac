23c737e4bb3c792a5e22409bc99b15deb53f6f76
Dongw1126 <topblade71@naver.com>
2021-12-29 02:05:21 +0900

    feat: file type validation

--- src/components/modal/WorkbookImageModal.tsx
+++ src/components/modal/WorkbookImageModal.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import { DialogContent, DialogTitle, Dialog, DialogActions, Button } from '@mui/material';
 import { Input } from '@mui/material';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 
 import { myPageChangeFlag } from "../../stores/DataChangeFlagStore";
 import * as Constants from "../../constants";
@@ -19,28 +16,39 @@ interface Props {
     onClose: () => void;
 }
 
-const fileValidation = (_file: any) => {
-    console.log(_file);
-}
-
-const fileUploadS3 = async (e: any) => {
-    const file = e.target.files[0];
-    fileValidation(file);
-    /*try {
-        await Storage.put(file.name, file, {
-            contentType: "image/png", // contentType is optional
-        });
-    } catch (error) {
-        alert("파일 업로드 오류");
-        console.log("Error uploading file: ", error);
-    }*/
-}
-
 /**
  * 문제집 표지 수정 Modal 창
  */
 function WorkbookImageModal(props: Props) {
     const dataChangeFlag = myPageChangeFlag;
+
+    const [error, setError] = useState(false);
+    const [errMsg, setErrMsg] = useState("");
+
+    const fileValidation = (_file: any) => {
+        console.log(_file);
+        if (!_file.name.match(/\.(jpg|jpeg|png|gif)$/)) {
+            setError(true);
+            setErrMsg("이미지가 아닙니다. (jpg, jpeg, png, gif)");
+        } else {
+            setError(false);
+            setErrMsg("");
+        }
+    }
+    
+    const fileUploadS3 = async (e: any) => {
+        const file = e.target.files[0];
+        fileValidation(file);
+        /*try {
+            await Storage.put(file.name, file, {
+                contentType: "image/png", // contentType is optional
+            });
+        } catch (error) {
+            alert("파일 업로드 오류");
+            console.log("Error uploading file: ", error);
+        }*/
+    }
+
 
     const handleClose = () => {
         props.onClose();
@@ -55,7 +63,14 @@ function WorkbookImageModal(props: Props) {
             <Dialog onClose={handleClose} open={props.open} >
                 <DialogTitle>문제집 표지 바꾸기</DialogTitle>
                 <DialogContent>
-                    <Input type="file" inputProps={{ accept: '.gif, .jpg, .png'}} onChange={fileUploadS3}/>
+                    <FormControl error={error}>
+                        <Input 
+                            type="file" 
+                            inputProps={{ accept: '.gif, .jpg, .jpeg, .png'}} 
+                            onChange={fileUploadS3}
+                        />
+                        <FormHelperText>{errMsg}</FormHelperText>
+                    </FormControl>
                 </DialogContent>
                 <DialogActions>
                     <Button variant="contained" onClick={handleEvent}>변경</Button>
