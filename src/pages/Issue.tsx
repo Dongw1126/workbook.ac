c89c5ff88bc39b8034498db10a10dbd01d2aa83f
Dongw1126 <topblade71@naver.com>
2022-01-20 15:41:02 +0900

    fix: issue link

--- src/pages/Issue.tsx
+++ src/pages/Issue.tsx
@@ -5,7 +5,7 @@ function Issue() {
         <div className={styles.issueContainer}>
             <p className={styles.title}>건의하기</p>
             <p className={styles.content}>
-                새 기능이나 버그에 대한 이야기는 <a href="https://github.com/Dongw1126/workbook.ac-issues/discussions">Github Discussion</a>에서 이루어지며, 누구나 자유롭게 이용하실 수 있습니다.<br/>
+                새 기능이나 버그에 대한 이야기는 <a href="https://github.com/Dongw1126/workbook.ac-issues/issues">Github</a>을 통해 이루어지며, 누구나 자유롭게 이용하실 수 있습니다.<br/>
                 그 밖에 문의는 <a href="mailto:workbookac2021@gmail.com">workbookac2021@gmail.com</a> 로 메일 주시면 감사하겠습니다.<br/>
             </p>
             {/*<br/><br/><br/><br/><br/><br/>
